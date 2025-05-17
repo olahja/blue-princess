ea30ff5b8b3911f73906a6d4e8a5a1b6a8bc40c7
Ola Hjalmarsson <ola.hja@gmail.com>
2025-05-18 01:20:12 +0200

    grejer

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,39 @@
   import Test from "./Test.svelte";
   import background from "./assets/background/background_plain.png";
   import title from "./assets/background/title.png";
-  import { fade , fly} from "svelte/transition";
+  import { fade, fly } from "svelte/transition";
   import CharacterCreator from "./lib/CharacterCreator.svelte";
 
-  // let enterOpts = {
-  //   inDelay: 1000,
-  //   inDuration: 8000,
-  //   outDuration: 2000,
-  // };
   let enterOpts = {
-    inDelay: 100,
-    inDuration: 800,
-    outDuration: 200,
+    inDelay: 1000,
+    inDuration: 8000,
+    outDuration: 2000,
   };
+  // let enterOpts = {
+  //   inDelay: 100,
+  //   inDuration: 800,
+  //   outDuration: 200,
+  // };
 
   let introOver = $state(false);
   let characterCreation = $state({
     hasStarted: false,
   });
+
+  const mainCharacters = import.meta.glob(
+    "./assets/characters/main/*.{png,jpg,jpeg,svg}",
+    {
+      eager: true,
+      as: "url",
+    },
+  );
+  const sidekicks = import.meta.glob(
+    "./assets/characters/sidekick/*.{png,jpg,jpeg,svg}",
+    {
+      eager: true,
+      as: "url",
+    },
+  );
 </script>
 
 <!-- <Test /> -->
@@ -29,7 +44,8 @@
     <!-- svelte-ignore a11y_img_redundant_alt -->
 
     {#if !introOver}
-      <img
+      <div
+        class="title"
         in:fade|global={{
           duration: enterOpts.inDuration,
           delay: enterOpts.inDelay,
@@ -39,24 +55,30 @@
           duration: enterOpts.outDuration,
           easing: quadOut,
         }}
-        class="title"
-        src={title}
-        alt="Description of my image"
-      />
+      >
+        <img src={title} alt="Description of my image" />
+        <span>– a very short minigame</span>
+      </div>
     {/if}
     {#if characterCreation.hasStarted}
-        <div class="character-creator-container"
+      <div
+        class="character-creator-container"
         in:fly|global={{
           y: -600,
           duration: 2000,
           delay: enterOpts.outDuration,
           easing: elasticOut,
         }}
-        >
-        
-        <CharacterCreator />
-
-        </div>
+        out:fly|global={{
+          y: 600,
+          duration: 750,
+          delay: enterOpts.outDuration,
+          easing: quadIn,
+        }}
+      >
+        <CharacterCreator characters={mainCharacters} heading={"Pick your character:"} />
+        <!-- <CharacterCreator characters={sidekicks} heading={"Pick your sidekick:"} /> -->
+      </div>
     {/if}
   </div>
   {#if !introOver}
@@ -98,6 +120,12 @@
     width: 100%;
     position: absolute;
     top: 5%;
+    text-align: center;
+    font-style: italic;
+    line-height: 1;
+    img {
+      width: 100%;
+    }
   }
   .character-creator-container {
     width: 100%;
@@ -110,7 +138,7 @@
     background-position: center;
     position: relative;
     max-width: 600px;
-    height: 500px;
+    height: 600px;
     margin: 0 auto;
     display: flex;
     align-items: center;
--- src/lib/CharacterCreator.svelte
+++ src/lib/CharacterCreator.svelte
@@ -1,51 +1,77 @@
 <script>
     let index = $state(0);
 
-    const mainCharacters = import.meta.glob(
-        "../assets/characters/main/*.{png,jpg,jpeg,svg}",
-        {
-            eager: true,
-            as: "url",
-        },
-    );
+    
+    let {characters, heading} = $props();
+
     const characterDescriptions = {
         cleaner: {
-            name: "The Cleaner",
-            desc: "Coming soon...",
+            name: "The New-Fridge-Lover",
+            desc: "Loves her new fridge almost as much as she loves solving puzzles.",
         },
         gardner: {
             name: "The Gardner",
-            desc: "Coming soon...",
+            desc: "Loves flowers. And puzzles. And flowers. And puzzles.",
         },
         puzzle_solver: {
             name: "The Puzzle Solver",
             desc: "Master at solving even the hardest of puzzles.",
         },
+        pyro: {
+            name: "The Pyromaniac",
+            desc: "He didn't start the fire... did he?",
+        },
+        professor: {
+            name: "The Professor",
+            desc: "He will find the cure for baldness. At any cost.",
+        },
+        samurai: {
+            name: "The Samurai",
+            desc: "Not to be messed with.",
+        },
     };
-    $inspect({ mainCharacters });
-    const mainCharacterList = Object.entries(mainCharacters).map(
-        ([path, src]) => ({
-            title: path,
-            src: src,
-            alt: path,
-            key: path.split("/").at(-1).replace(".png", ""),
-        }),
-    );
-    const images = mainCharacterList;
+    $inspect({ characters });
+    const characterList = Object.entries(characters).map(([path, src]) => ({
+        title: path,
+        src: src,
+        alt: path,
+        key: path.split("/").at(-1).replace(".png", ""),
+    }));
+    const images = characterList;
+
+    function scrollIntoView(event) {
+		event.currentTarget.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+	}
+
+    let selectedCharacter = $state()
+
+    function handleCharacterClick(event, key) {
+        selectedCharacter = key
+        scrollIntoView(event)
+
+    }
 
     $inspect({ images });
 </script>
 
 <div class="car-container">
-    {#each images as image}
-        <figure>
-            <img src={image.src} alt={image.alt} />
-            <figcaption>
-                <h2>{characterDescriptions[image.key].name}</h2>
-                <span>{characterDescriptions[image.key].desc}</span>
-            </figcaption>
-        </figure>
-    {/each}
+    <h1>{heading}</h1>
+    <div class="character-container">
+        {#each images as image}
+            <!-- svelte-ignore a11y_click_events_have_key_events -->
+            <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+            <figure class:selected={selectedCharacter == image.key} onclick={(e) => handleCharacterClick(e, image.key)}>
+                <img src={image.src} alt={image.alt} />
+                <figcaption>
+                    <h2>{characterDescriptions[image.key].name}</h2>
+                    <span style:font-style="italic">{characterDescriptions[image.key].desc}</span>
+                </figcaption>
+            </figure>
+        {/each}
+    </div>
+    <div class="button-container">
+        <button disabled={selectedCharacter == undefined}>Confirm</button>
+    </div>
 </div>
 
 <!-- <Carousel bind:index>
@@ -56,11 +82,31 @@
     </Carousel> -->
 
 <style>
+    .character-container {
+        display: flex;
+        overflow-x: scroll;
+        gap: 10px;
+    }
+    .button-container {
+        margin-top: 10px;
+    }
+    
     figure {
+        cursor: pointer;
         border-radius: 4px;
         border: 1px solid #ccc;
         backdrop-filter: blur(22px);
         -webkit-backdrop-filter: blur(22px);
+        min-width: 280px;
+        margin: 0;
+        margin-bottom: 14px;
+        &.selected {
+            border: 4px solid #90EE90;
+        }
+
+    }
+    figcaption {
+        padding: 10px 6px;
     }
     .car-container {
         background: rgba(255, 255, 255, 0.2);
@@ -71,11 +117,16 @@
         -webkit-backdrop-filter: blur(2px);
         border: 1px solid rgba(255, 255, 255, 0.3);
         text-align: center;
+        line-height: 1;
     }
     h2 {
         margin: 0;
         margin-bottom: 5px;
         line-height: 1;
+        font-size: 18px;
+    }
+    h1 {
+        font-size: 24px;
     }
     img {
         width: 100%;
